1178da3b5d52266d68739136d5184852b0ce8163
Jannis <js.muething@gmail.com>
2014-05-16 11:56:56 +0200

    session in group

--- MultiMeet/Group.h
+++ MultiMeet/Group.h
@@ -7,11 +7,13 @@
 //
 
 #import <Foundation/Foundation.h>
+@import MultipeerConnectivity;
 
 @interface Group : NSObject
 
-@property(nonatomic) NSInteger numberOfPeople;
+@property(nonatomic) MCSession *session;
 
+@property(nonatomic) NSInteger numberOfPeople;
 @property(nonatomic) NSSet *usersInGroup;
 
 @end
